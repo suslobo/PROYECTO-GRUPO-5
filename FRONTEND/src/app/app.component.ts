6d102f215e9f28fac07dda76847ca95e60486f34
almudenadomenech <violeta_33@hotmail.es>
2024-03-16 19:18:58 +0100

    app.router y logo

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HouseDetailComponent } from './house-detail/house-detail.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HouseFormComponent } from './house-form/house-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { BookingFormComponent } from './booking-form/booking-form.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { BookingListComponent } from './booking-list/booking-list.component';
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, RouterLink, HouseDetailComponent,
+      NavbarComponent, HouseFormComponent, FooterComponent,
+      BookingFormComponent, NotFoundComponent, UserListComponent, BookingListComponent]
 })
 export class AppComponent {
-  title = 'FRONTEND';
+  title = 'FRONTED';
 }
--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -1,3 +1,81 @@
 import { Routes } from '@angular/router';
+import { HouseListComponent } from './house-list/house-list.component';
+import { HouseDetailComponent } from './house-detail/house-detail.component';
+import { BookingListComponent } from './booking-list/booking-list.component';
+import { BookingDetailComponent } from './booking-detail/booking-detail.component';
+import { HomeComponent } from './home/home.component';
+import { BookingFormComponent } from './booking-form/booking-form.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HouseFormComponent } from './house-form/house-form.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'houses',
+        component: HouseListComponent
+    },
+    {
+        path: 'houses/:id/detail',
+        component: HouseDetailComponent
+    },
+    {
+        path: 'booking',
+        component: BookingListComponent
+    },
+    {
+        path: 'booking/:id/detail',
+        component: BookingDetailComponent
+    },
+    {
+        path: 'booking/:id/form',
+        component: BookingFormComponent
+    },
+    {
+        path: 'houses/create',
+        component: HouseFormComponent
+    },
+    {
+        path: 'houses/:id/update',
+        component: HouseFormComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'user',
+        component: UserListComponent
+    },
+    {
+        path: 'user/profile',
+        component: UserProfileComponent
+    },
+    {
+        path: 'not-found-404',
+        component: NotFoundComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/not-found-404'
+    }
+
+
+];
+
