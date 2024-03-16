ddd5a4ccf8abbe559f49ae243d97f096e61b2377
almudenadomenech <violeta_33@hotmail.es>
2024-03-16 19:27:42 +0100

    house-list

--- FRONTEND/src/app/house-list/house-list.component.ts
+++ FRONTEND/src/app/house-list/house-list.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { House } from '../interfaces/house.model';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-house-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './house-list.component.html',
   styleUrl: './house-list.component.css'
 })
-export class HouseListComponent {
+export class HouseListComponent implements OnInit{
+
+  houses: House [] = [];
+
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this.httpClient.get<House[]>('http://localhost:3000/houses')
+    .subscribe(houses => this.houses = houses);
+  }
+
+  deleteById(id: string | number): void {
+    const remove: boolean = confirm("¿Quiere eliminar esta casa?");
+    if (!remove) return;
+    this.httpClient.delete<House>(`http://localhost:3000/houses/${id}`)
+      .subscribe(() => {
+
+        console.log('La casa ha sido eliminada');
+
+       this.houses = this.houses.filter(house => house.id !== id);
+      });
+  }
+
 
 }
+
