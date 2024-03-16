0946106a225c0c2af581b124a43e5d17c3238e41
almudenadomenech <violeta_33@hotmail.es>
2024-03-16 19:30:08 +0100

    house detail

--- FRONTEND/src/app/house-detail/house-detail.component.ts
+++ FRONTEND/src/app/house-detail/house-detail.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { House } from '../interfaces/house.model';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-house-detail',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './house-detail.component.html',
   styleUrl: './house-detail.component.css'
 })
-export class HouseDetailComponent {
+export class HouseDetailComponent implements OnInit {
+
+  house: House | undefined;
+
+
+  constructor(private http: HttpClient,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      this.http.get<House>(`http://localhost:3000/houses/${id}`).subscribe(house => this.house = house);
+    }
+    );
+  }
+
+
 
 }
+
