49a505fe85d4d97ddda02cf866cd9baa5cade99a
almudenadomenech <violeta_33@hotmail.es>
2024-03-16 19:33:49 +0100

    booking-list

--- FRONTEND/src/app/booking-list/booking-list.component.ts
+++ FRONTEND/src/app/booking-list/booking-list.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Booking } from '../interfaces/booking.model';
 
 @Component({
   selector: 'app-booking-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, HttpClientModule],
   templateUrl: './booking-list.component.html',
   styleUrl: './booking-list.component.css'
 })
-export class BookingListComponent {
+export class BookingListComponent implements OnInit{
+
+  bookings: Booking [] = [];
+
+  constructor(private httpClient: HttpClient){}
+  ngOnInit(): void {
+    this.httpClient.get<Booking[]>('http://localhost:3000/booking')
+    .subscribe(bookings => this.bookings = bookings);
+  }
+
+  deleteById(id: string | number): void {
+    const remove: boolean = confirm("¿Quiere eliminar esta reserva de su lista?");
+    if (!remove) return;
+    this.httpClient.delete<Booking>(`http://localhost:3000/booking/${id}`)
+      .subscribe(() => {
+
+       this.bookings = this.bookings.filter(booking => booking.id !== id);
+      });
+  }
 
 }
+
