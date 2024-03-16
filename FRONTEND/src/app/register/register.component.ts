a1296b16c4ea0e535b13f7fb046f013dd34d9463
almudenadomenech <violeta_33@hotmail.es>
2024-03-16 19:22:25 +0100

    register

--- FRONTEND/src/app/register/register.component.ts
+++ FRONTEND/src/app/register/register.component.ts
@@ -1,12 +1,57 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Register } from '../interfaces/register.models';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  registerForm = this.fb.group({
+    nickname: ['', [ Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', [Validators.required, Validators.pattern('^[0-9]{9}$')]],
+    password: ['', [Validators.required]],
+    passwordConfirm: ['', [Validators.required]]
+  },
+  {validators: this.passwordConfirmValidator}
+  );
+
+  constructor(private fb: FormBuilder,
+    private httpClient: HttpClient) {}
+
+    passwordConfirmValidator(control: AbstractControl){
+      if(control.get('password')?.value === control.get('passwordConfirm')?.value){
+        return null;
+      } else {
+        return {
+          'confirmError': true
+        }
+
+
+      }
+    }
+
+  save(){
+
+    let register: Register = {
+      nickname: this.registerForm.get('nickname')?.value ?? '',
+      email: this.registerForm.get('email')?.value ?? '',
+      phone: this.registerForm.get('phone')?.value ?? '',
+      password: this.registerForm.get('password')?.value ?? ''
+    };
+    let url= 'http://localhost:3000/register';
+    this.httpClient.post<Register>(url, register)
+    .subscribe(respuesta => {
+      console.log(respuesta);
+
+    });
+  }
 }
+
