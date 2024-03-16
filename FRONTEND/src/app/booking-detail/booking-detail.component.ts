ec8d0a97a0c7f78784728e3ab4912f372d8953e5
almudenadomenech <violeta_33@hotmail.es>
2024-03-16 19:36:43 +0100

    booking detail

--- FRONTEND/src/app/booking-detail/booking-detail.component.ts
+++ FRONTEND/src/app/booking-detail/booking-detail.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Booking } from '../interfaces/booking.model';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-booking-detail',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './booking-detail.component.html',
   styleUrl: './booking-detail.component.css'
 })
-export class BookingDetailComponent {
+export class BookingDetailComponent implements OnInit {
+
+  booking: Booking | undefined;
+
+  constructor (private activatedRoute: ActivatedRoute,
+    private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      this.httpClient.get<Booking>(`http://localhost:3000/booking/${id}`)
+      .subscribe(booking => this.booking = booking);
+    })
+  }
 
 }
+
