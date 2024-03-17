5b726ef396677eb004e627a07b422d975c2c8f27
suslobo <148755303+suslobo@users.noreply.github.com>
2024-03-17 12:35:35 +0100

    creación interfaz booking backend

--- backend/src/Booking/Booking.model.ts
+++ backend/src/Booking/Booking.model.ts
@@ -1,6 +1,20 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Booking {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: 'date'})
+    entryDate: Date;
+
+    @Column({type: 'date'})
+    departureDate: Date;
+
+    @Column()
+    people: number;
+
+    @Column()
+    available: boolean;
 
 }
