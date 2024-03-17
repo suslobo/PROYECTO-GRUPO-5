59d34996b237f573d46b7a4904249e3c96ad7a3a
almudenadomenech <violeta_33@hotmail.es>
2024-03-17 12:10:33 +0100

    controladores booking, user y house

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HouseController } from './house/house.controller';
+import { BookingController } from './booking/booking.controller';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HouseController, BookingController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
