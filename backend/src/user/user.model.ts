5621e29f59aed91d5fadfbd983dd89d39dfec5cb
almudenadomenech <violeta_33@hotmail.es>
2024-03-17 12:27:05 +0100

    user model

--- backend/src/user/user.model.ts
+++ backend/src/user/user.model.ts
@@ -1,8 +1,47 @@
-import { Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Role } from "./role.model";
 
 
 @Entity()
 export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({unique: true})
+    email: string;
+
+    @Column()
+    phone: string;
+
+    @Column({unique: true})
+    nif: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    street: string;
+
+    @Column()
+    city: string;
+
+    @Column()
+    postalCode: string;
+
+    @Column()
+    role: Role;
+
+
+
+
+
 
 
    
