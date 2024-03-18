e28051994e32fb75c4f4bc03e59178ea2ff2e6d5
Ekagarcian <156067063+Ekagarcian@users.noreply.github.com>
2024-03-18 23:14:56 +0100

    user detail terminado

--- FRONTEND/src/app/user-detail/user-detail.component.ts
+++ FRONTEND/src/app/user-detail/user-detail.component.ts
@@ -1,37 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../interfaces/user.model';
-import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
+  standalone: true,
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css']
+  styleUrl: './user-detail.component.css'
 })
 export class UserDetailComponent implements OnInit {
+deleteUser() {
+throw new Error('Method not implemented.');
+}
 
   user: User | undefined;
-  userService: any;
 
   constructor(private http: HttpClient,
-              private activatedRoute: ActivatedRoute,
-              private router: Router) { }
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
       let id = params['id'];
       this.http.get<User>(`http://localhost:3000/users/${id}`).subscribe(user => this.user = user);
-    });
-  }
-
-  deleteUser() {
-    const remove = confirm("¿Quiere eliminar este usuario?");
-    if (!remove || !this.user) return;
-
-    this.userService.deleteById(this.user.id).subscribe(() => {
-      // Navegar hacia la lista de usuarios después de eliminar
-      this.router.navigate(['/usuarios']);
     });
   }
 }
