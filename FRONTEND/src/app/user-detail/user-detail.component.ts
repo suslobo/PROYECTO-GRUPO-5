bfc50a3de3c3a904d5bf64d0efcc154a49fa0b03
Ekagarcian <156067063+Ekagarcian@users.noreply.github.com>
2024-03-18 19:27:06 +0100

    user detail ts

--- FRONTEND/src/app/user-detail/user-detail.component.ts
+++ FRONTEND/src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user.model';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../services/user.service'; // Agrega la importación del servicio UserService
 
 @Component({
   selector: 'app-user-detail',
-  standalone: true,
-  imports: [],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.css'
+  styleUrls: ['./user-detail.component.css']
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
 
+  user: User | undefined;
+
+  constructor(private http: HttpClient,
+              private userService: UserService, // Inyecta el servicio UserService
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      this.http.get<User>(`http://localhost:3000/users/${id}`).subscribe(user => this.user = user);
+    });
+  }
+
+  deleteUser() {
+    const remove = confirm("¿Quiere eliminar este usuario?");
+    if (!remove || !this.user) return;
+
+    this.userService.deleteById(this.user.id).subscribe(() => {
+      // Navegar hacia la lista de usuarios después de eliminar
+      this.router.navigate(['/usuarios']);
+    });
+  }
 }
