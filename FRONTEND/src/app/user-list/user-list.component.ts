8db446f57facb1734083c121db7a3549596292e6
Ekagarcian <156067063+Ekagarcian@users.noreply.github.com>
2024-03-18 12:37:40 +0100

    Cammbio html y ts

--- FRONTEND/src/app/user-list/user-list.component.ts
+++ FRONTEND/src/app/user-list/user-list.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-list',
-  standalone: true,
-  imports: [],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrls: ['./user-list.component.css'] // Corrección en la propiedad styleUrls
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users: User[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.http.get<User[]>('http://localhost:3000/users')
+      .subscribe(users => this.users = users);
+  }
+
+  deleteUser(id: number): void {
+    // Mostrar una confirmación preguntando si se quiere borrar el usuario
+    const remove: boolean = confirm("¿Quiere borrar el usuario de verdad?");
+
+    if (!remove) return; // Si el usuario no confirma, no se borra
+    
+    // Aquí puedes implementar la lógica para borrar el usuario con el ID proporcionado
+  }
 }
