76de9dc900093d5589883ba80c742b5d21a6eea6
suslobo <148755303+suslobo@users.noreply.github.com>
2024-03-20 19:17:41 +0100

    fullstack creación de house.controller.ts y house.model.ts

--- backend/src/house/house.model.ts
+++ backend/src/house/house.model.ts
@@ -7,7 +7,58 @@ export class House {
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Column({nullable: false})
+    title: string;
+
+    @Column()
+    address: string;
+
+    @Column()
+    phone: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    places: string;
+
+    @Column()
+    bedrooms: number;
+
+    @Column()
+    bathrooms: number;
+
+    @Column()
+    price: number;
+
+    @Column()
+    meters: number;
+
+    @Column()
+    destination: string;
+
+    @Column()
+    petFriendly: boolean;
+
+    @Column()
+    pool: boolean;
+
+    @Column()
+    garden: boolean;
+
+    @Column()
+    terrace: boolean;
+
+    @Column()
+    wifi: boolean;
+
+    @Column()
+    air: boolean;
+
+    @Column({length: 1000})
+    description: string;
+
     @Column({type: 'simple-array'})
-    photos: string[];
+    photoUrls: string[];
     
 }
