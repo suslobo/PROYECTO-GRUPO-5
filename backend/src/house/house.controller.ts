657ed406c8d3589ade2a330ed70041541ac57585
almudenadomenech <violeta_33@hotmail.es>
2024-03-23 14:08:08 +0100

    modificacion house.controller

--- backend/src/house/house.controller.ts
+++ backend/src/house/house.controller.ts
@@ -1,91 +1,198 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Between, Repository } from 'typeorm';
 import { House } from './house.model';
+import { Repository } from 'typeorm';
 
 @Controller('house')
 export class HouseController {
 
-    constructor(
-        @InjectRepository(House) private houseRepository: Repository<House>
-    ){}
+    constructor(@InjectRepository(House) private houseRepo: Repository<House>) {}
 
     @Get()
     findAll() {
-        return this.houseRepository.find();
+        return this.houseRepo.find();
     }
 
-    /* @Get('filter-by-id/:id') 
-    findById(@Param('id', ParseIntPipe) id: number ) {
-        return this.houseRepository.findOne({
-           
+
+    @Get(':id')
+    findById(@Param('id', ParseIntPipe) id: number) {
+        return this.houseRepo.findOne({
             where: {
                 id: id
             }
         });
     }
-        
 
-    @Get('filter-by-title/:title')
-    findByTitle(@Param('title') title: string) {
-        return this.houseRepository.find({
+    @Get('filter-by-title')
+    findByTitle(@Param('id', ParseIntPipe) id: string) {
+        return this.houseRepo.findOne({
             where: {
-                title: title 
+                title: id
             }
         });
     }
 
-    
+    @Get('filter-by-address')
+    findByAdress(@Param('id', ParseIntPipe) id: string) {
+        return this.houseRepo.findOne({
+            where: {
+                address: id
+            }
+        });
+    }
 
+    @Get('filter-by-phone')
+    findByPhone(@Param('id', ParseIntPipe) id: string) {
+        return this.houseRepo.findOne({
+            where: {
+                phone: id
+            }
+        });
+    }
+
+    @Get('filter-by-places')
+    findByPlaces(@Param('id', ParseIntPipe) id: string) {
+        return this.houseRepo.findOne({
+            where: {
+                places: id
+            }
+        });
+    }
+
+    @Get('filter-by-bedrooms')
+    findByBedrooms(@Param('id', ParseIntPipe) id: number) {
+        return this.houseRepo.findOne({
+            where: {
+                bedrooms: id
+            }
+        });
+    }
+
+    @Get('filter-by-bathrooms')
+    findByBathrooms(@Param('id', ParseIntPipe) id: number) {
+        return this.houseRepo.findOne({
+            where: {
+                bathrooms: id
+            }
+        });
+    }
+
+    /*
     @Get('filter-by-price/:min/:max')
     findByPriceBetweenMinAndMax(
         @Param('min', ParseIntPipe) min: number,
-        @Param('max', ParseIntPipe) max: number
-    ) {
-        return this.houseRepository.find({
+        @Param('max', ParseIntPipe) max: number) {
+        return this.houseRepo.find({
             where: {
                 price: Between(min, max)
+            }
+        });
+    }
+    */
+
+    @Get('filter-by-price')
+    findByPrice(@Param('id', ParseIntPipe) id: number) {
+        return this.houseRepo.findOne({
+            where: {
+                price: id
+            }
+        });
+    }
+
+    @Get('filter-by-meters')
+    findBymeters(@Param('id', ParseIntPipe) id: number) {
+        return this.houseRepo.findOne({
+            where: {
+                meters: id
+            }
+        });
+    }
+
+    @Get('filter-by-destination')
+    findByDestination(@Param('id', ParseIntPipe) id: string) {
+        return this.houseRepo.findOne({
+            where: {
+                destination: id
+            }
+        });
+    }
+
+    @Get('filter-by-petFriendly')
+    findByPetFriendly(@Param('id', ParseIntPipe) id: boolean) {
+        return this.houseRepo.find({
+            where: {
+                petFriendly: id
+            }
+        });
+    }
+
+    @Get('filter-by-pool')
+    findByPool(@Param('id', ParseIntPipe) id: boolean) {
+        return this.houseRepo.find({
+            where: {
+                pool: id
+            }
+        });
+    }
+
+    @Get('filter-by-garden')
+    findByGarden(@Param('id', ParseIntPipe) id: boolean) {
+        return this.houseRepo.find({
+            where: {
+                garden: id
+            }
+        });
+    }
+
+    @Get('filter-by-terrace')
+    findByTerrace(@Param('id', ParseIntPipe) id: boolean) {
+        return this.houseRepo.find({
+            where: {
+                terrace: id
+            }
+        });
+    }
+
+    @Get('filter-by-wifi')
+    findByWifi(@Param('id', ParseIntPipe) id: boolean) {
+        return this.houseRepo.find({
+            where: {
+                wifi: id
+            }
+        });
+    }
+
+    @Get('filter-by-air')
+    findByAir(@Param('id', ParseIntPipe) id: boolean) {
+        return this.houseRepo.find({
+            where: {
+                air: id
+            }
+        });
+    }
+
+    @Get('filter-by-description')
+    findByDescription(@Param('id', ParseIntPipe) id: string) {
+        return this.houseRepo.find({
+            where: {
+                description: id
+            }
+        });
+    }
+
+    @Get('filter-by-photoUrls')
+    findByPhotoUrls(@Param('id', ParseIntPipe) id: string) {
+        return this.houseRepo.find({
+            where: {
+                photoUrls: id
             }
         });
     }
 
     @Post()
     create(@Body() house: House) {
-        return this.houseRepository.save(house);
+        return this.houseRepo.save(house);
     }
 
-   
-    @Put(':id')
-    async update(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() house: House
-        ) {
-            
-            const exists = await this.houseRepository.existsBy({
-               id: id
-            });
-
-            if(!exists) {
-                throw new NotFoundException('Book not found');
-            }
-
-            return this.houseRepository.save(house);
-
-    }
-
-    
-    @Delete(':id')
-    async deleteById(
-        @Param('id', ParseIntPipe) id: number
-    ) {
-
-        const exists = await this.houseRepository.existsBy({
-            id: id
-         });
-
-         if(!exists) {
-             throw new NotFoundException('House not found');
-         }        
-        
-    } */
 }
+
