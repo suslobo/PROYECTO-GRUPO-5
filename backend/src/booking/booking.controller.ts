7d723f99d51edfa6f47ce8072ba30bfd73493f13
almudenadomenech <violeta_33@hotmail.es>
2024-03-24 17:50:59 +0100

    backend booking.controller.ts añadido @delete

--- backend/src/booking/booking.controller.ts
+++ backend/src/booking/booking.controller.ts
@@ -2,7 +2,7 @@ import { Body, ConflictException, Controller, Delete, Get, NotFoundException, Pa
 import { InjectRepository } from '@nestjs/typeorm';
 import { Booking } from './booking.model';
 import { Repository } from 'typeorm';
-import { House } from 'src/houses/houses.model';
+
 
 @Controller('booking')
 export class BookingController {
@@ -53,6 +53,31 @@ export class BookingController {
         return this.bookingRepository.save(Booking);
     }
 
-    
+    @Delete(':id')
+    async deleteById(
+        @Param('id', ParseIntPipe) id: number
+    ) {
+
+        const exists = await this.bookingRepository.existsBy({
+            id: id
+         });
+
+         if(!exists) {
+             throw new NotFoundException('House not found');
+         }
+
+        try {
+           
+            const house = await this.bookingRepository.findOne({
+                where: {id: id}
+            });
+           
+            await this.bookingRepository.save(house);
+        } catch (error) {
+            console.log("Error al borrar la casa")
+            throw new ConflictException('No se puede borrar.');
+        }
+        
+    }
 
 }
