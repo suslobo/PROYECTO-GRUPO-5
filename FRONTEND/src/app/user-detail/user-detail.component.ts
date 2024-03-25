a403bcf6fe5f6ac3d5c4792f2e0c1fbc3b02c621
almudenadomenech <violeta_33@hotmail.es>
2024-03-25 15:33:23 +0100

    user-detail copiado de main

--- FRONTEND/src/app/user-detail/user-detail.component.ts
+++ FRONTEND/src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-user-detail',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './user-detail.component.html',
+//   styleUrl: './user-detail.component.css'
+// })
+// export class UserDetailComponent {
+
+// }
+import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user.model';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.css'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+deleteUser() {
+throw new Error('Method not implemented.');
+}
 
+  user: User | undefined;
+
+  constructor(private http: HttpClient,
+              private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      this.http.get<User>(`http://localhost:3000/users/${id}`).subscribe(user => this.user = user);
+    });
+  }
 }
