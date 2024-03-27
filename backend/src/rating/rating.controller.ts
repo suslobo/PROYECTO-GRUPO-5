6076f12575e598e4c4730066ec5b69c9f1c7d659
suslobo <148755303+suslobo@users.noreply.github.com>
2024-03-27 14:40:44 +0100

    Backend Controlador rating

--- backend/src/rating/rating.controller.ts
+++ backend/src/rating/rating.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Rating } from './rating.model';
 import { Repository } from 'typeorm';
@@ -10,4 +10,54 @@ export class RatingController {
         @InjectRepository(Rating)
         private ratingRepository: Repository<Rating>
     ) {}
+
+    @Get()
+    findAll() {
+        return this.ratingRepository.find()
+    }
+
+    @Get(':id') 
+    findById( @Param('id', ParseIntPipe) id: number ) {
+        return this.ratingRepository.findOne({
+            where: {
+                id: id
+            }
+        });
+    }
+
+    @Get('filter-by-user/:id')
+    findByUserId(@Param('id', ParseIntPipe) id: number){
+        return this.ratingRepository.find({
+            where: {
+                user: {
+                    id: id
+                }
+            }
+        });
+    }
+
+    @Post()
+    create(@Body() rating: Rating) {
+        return this.ratingRepository.save(rating);
+    }
+
+    @Put(':id')
+    async update(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() rating: Rating
+        ) {
+            
+            // await espera a que el método existsBy termine ya que devuelve Promise<boolean>
+            const exists = await this.ratingRepository.existsBy({
+               id: id
+            });
+
+            if(!exists) {
+                throw new NotFoundException('Rating not found');
+            }
+
+            return this.ratingRepository.save(rating);
+    }
+
 }
+
