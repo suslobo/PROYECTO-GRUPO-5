ad004618e2c36dac2b2df2948b0a334fb5cf7b4d
almudenadomenech <violeta_33@hotmail.es>
2024-03-31 11:35:01 +0200

    main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const config = new DocumentBuilder().setTitle("BACKEND NEST").build();
+  const config = new DocumentBuilder().setTitle("BACKEND").build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  
+  
+ 
+
+  // Configurar carpeta para subida imágenes/archivos
+  // http://localhost:3000/uploads/1f82d390-d902-4aed-ad23-d543f56f2433.png
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+  
   await app.listen(3000);
 }
 bootstrap();
