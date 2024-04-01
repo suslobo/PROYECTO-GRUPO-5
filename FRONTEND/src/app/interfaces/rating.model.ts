78340e7108285f5ade2e403614a6385af2096700
suslobo <148755303+suslobo@users.noreply.github.com>
2024-04-01 12:54:47 +0200

    Frontend corrección en la interface rating

--- FRONTEND/src/app/interfaces/rating.model.ts
+++ FRONTEND/src/app/interfaces/rating.model.ts
@@ -5,28 +5,10 @@ export interface Rating {
 
     id: number;
 
-  title: string;
-  address: string;
-  phone: string;
-  email: string;
-  places: string;
-  bedrooms: number;
-  bathrooms: number;
-  price: number;
-
-  meters: number;
-  destination: string;
-
-  petFriendly: boolean;
-  pool: boolean;
-  garden: boolean;
-  terrace: boolean;
-  wifi: boolean;
-  air: boolean;
-
-  description: string;
-  photoUrls: string [];
-
-  user?: User;
-  book?: House;
+  score: number;
+  comment: string;
+  createdDate: Date;
+  userId: string;
+  houseId: number;
+  
 }
