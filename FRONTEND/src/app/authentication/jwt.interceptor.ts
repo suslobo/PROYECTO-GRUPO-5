add6521a6a20bf5fef46d67f345f8bc841ad9fbd
Almudena Almu <violeta_33@hotmail.es>
2024-04-13 12:13:19 +0200

    jwt interceptor

--- FRONTEND/src/app/authentication/jwt.interceptor.ts
+++ FRONTEND/src/app/authentication/jwt.interceptor.ts
@@ -1,15 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
+
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
 
   const token = localStorage.getItem("jwt_token");
-  if(token) {
+  if(token){
     req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
+      headers: req.headers.set('Authorization',  `Bearer ${token}`)
+     
     });
   }
-
   return next(req);
 };
+
