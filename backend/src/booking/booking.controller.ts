0a686455eecff44eabd3bfc10b5bb14263f45d34
Almudena Almu <violeta_33@hotmail.es>
2024-04-13 15:12:00 +0200

    booking controler modificado

--- backend/src/booking/booking.controller.ts
+++ backend/src/booking/booking.controller.ts
@@ -1,4 +1,4 @@
-import { Body, ConflictException, Controller, Delete, Get, NotFoundException, Param, Request, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common';
+/* import { Body, ConflictException, Controller, Delete, Get, NotFoundException, Param, Request, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Booking } from './booking.model';
 import { Repository } from 'typeorm';
@@ -17,7 +17,7 @@ export class BookingController {
     @Get()
     findAll(){
         return this.bookingRepository.find();
-    }
+    } */
 
    /*  @Get('filter-by-id/:id')
     findById(@Param('id', ParseIntPipe) id:number){
@@ -27,7 +27,7 @@ export class BookingController {
             }
         });
     } */
-
+/* 
    @Get('filter-by-user/:email')
   
     findByUserId(@Param('email') email: string){
@@ -56,7 +56,7 @@ export class BookingController {
             });
         }
 
-    }   
+    }    */
 
   /*   @Get('filter-by-destination')
     findByBookId(@Param('id', ParseIntPipe) id: string){
@@ -66,9 +66,9 @@ export class BookingController {
             }
         });
     }  */
-    @Get(':id')
+    // @Get(':id')
     //@UseGuards(AuthGuard('jwt'))
-    findById(@Param('id', ParseIntPipe) id: number) {
+ /*    findById(@Param('id', ParseIntPipe) id: number) {
        return this.bookingRepository.findOne({
             where: {
                id: id
@@ -85,6 +85,91 @@ export class BookingController {
     }
 
     
+    @Delete(':id')
+async deleteById(
+    @Param('id', ParseIntPipe) id: number
+) {
+    const exists = await this.bookingRepository.existsBy({ id: id });
+
+    if (!exists) {
+        throw new NotFoundException('Booking not found');
+    }
+
+    try {
+        await this.bookingRepository.delete(id);
+    } catch (error) {
+        console.log("Error al borrar la reserva", error);
+        throw new ConflictException('No se puede borrar.');
+    }
+}
+
+} */
+
+
+
+import { Body, ConflictException, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Booking } from './booking.model';
+import { Repository } from 'typeorm';
+
+
+@Controller('booking')
+export class BookingController {
+
+    constructor(
+        @InjectRepository(Booking)
+        private bookingRepository: Repository<Booking>
+    ){}
+
+    @Get()
+    findAll(){
+        return this.bookingRepository.find();
+    }
+
+   /*  @Get('filter-by-id/:id')
+    findById(@Param('id', ParseIntPipe) id:number){
+        return this.bookingRepository.findOne({
+            where: {
+                id: id
+            }
+        });
+    } */
+
+   @Get('filter-by-user/:email')
+    findByUserId(@Param('email') email: string){
+        return this.bookingRepository.find({
+            where: {
+                users: {
+                    email: email
+                }
+            }
+        });
+    } 
+
+  /*   @Get('filter-by-destination')
+    findByBookId(@Param('id', ParseIntPipe) id: string){
+        return this.bookingRepository.find({
+            where: {
+                destination: id
+            }
+        });
+    }  */
+    @Get(':id')
+    findById(@Param('id', ParseIntPipe) id: number) {
+       return this.bookingRepository.findOne({
+            where: {
+               id: id
+                }
+            });
+        
+}
+    @Post()
+    create(@Body() booking: Booking) {
+        return this.bookingRepository.save(booking);
+                
+    }
+
+
     @Delete(':id')
 async deleteById(
     @Param('id', ParseIntPipe) id: number
