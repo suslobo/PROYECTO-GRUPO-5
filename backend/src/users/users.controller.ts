4dfa643ab4df3453652a4d2a4f40e3f213f56c32
almudenadomenech <violeta_33@hotmail.es>
2024-04-15 19:35:44 +0200

    modificaciones

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, ConflictException, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put, Request, UnauthorizedException, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, ConflictException, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put, Request, UnauthorizedException, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './users.model';
@@ -7,6 +7,7 @@ import { Role } from './role.model';
 import { Login } from './login.dto';
 import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('users')
 export class UsersController {
@@ -56,6 +57,23 @@ async update(
         return this.userRepository.save(user);
 
 }
+
+@Post('avatar')
+    @UseInterceptors(FileInterceptor('file'))
+    @UseGuards(AuthGuard('jwt'))
+    async uploadAvatar(
+        @UploadedFile() file: Express.Multer.File,
+        @Request() request
+    ) {
+        if (!file) {
+            throw new BadRequestException('Archivo incorrecto')
+        }
+
+        // guardar la ruta del archivo, hay que crear un atributo del user
+        request.user.photoUrl = file.filename;
+        return await this.userRepository.save(request.user);
+
+    }
 
 
 
