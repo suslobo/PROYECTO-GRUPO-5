d20fc1f7e8586ce2b9692c24083f7bc38c2580d3
almudenadomenech <violeta_33@hotmail.es>
2024-04-15 19:25:17 +0200

    modificaciones user.controller

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, ConflictException, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put, Request, UnauthorizedException, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, ConflictException, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put, Request, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './users.model';
@@ -7,7 +7,6 @@ import { Role } from './role.model';
 import { Login } from './login.dto';
 import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
-import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('users')
 export class UsersController {
@@ -15,7 +14,7 @@ export class UsersController {
         @InjectRepository(User)
         private userRepository: Repository<User>,
         private jwtService: JwtService
-    ) { }
+    ){}
 
     @Get()
     findAll() {
@@ -23,13 +22,14 @@ export class UsersController {
     }
    @Get(':id')
     findById(@Param('id', ParseIntPipe) id: number) {
-        return this.userRepository.findOne({
+       return this.userRepository.findOne({
             where: {
                id: id
                 }
             });
         
 }
+
 
 @Get('account/:id')
 @UseGuards(AuthGuard('jwt'))
@@ -44,6 +44,7 @@ async update(
     @Body() user: User
     ) {
         
+        
         const exists = await this.userRepository.existsBy({
            id: id
         });
@@ -56,15 +57,14 @@ async update(
 
 }
 
-    }
-/*     @Get('filter-by-firstName')
-    findByTitle(@Param('id', ParseIntPipe) id: string) {
-        return this.userRepository.findOne({
-            where: {
-                firstName: id
-            }
-        });
-    } */
+
+
+@Post('register')
+async register(@Body() register: Register) {
+    
+    const exists = await this.userRepository.existsBy({
+        email: register.email
+    });
 
     if(exists)
         throw new ConflictException("Email ocupado");
@@ -83,41 +83,24 @@ async update(
     return await this.userRepository.save(user);
 }
 
-        if (exists)
-            throw new ConflictException("Email ocupado");
-
-
-        const user: User = {
-            id: 0,
-            nickName: register.nickName,
-            email: register.email,
-            password: register.password,
-            phone: '',
-
-            role: Role.USER,
-            photoUrl: ''
-        };
-        return await this.userRepository.save(user);
-    }
-
-    @Post('login')
+@Post('login')
     async login(@Body() login: Login) {
 
-
+       
         const exists = await this.userRepository.existsBy({
             email: login.email
         });
-        if (!exists)
+        if(!exists)
             throw new NotFoundException("Usuario no encontrado.");
 
-
+        
         const user = await this.userRepository.findOne({
             where: {
                 email: login.email
             }
         });
 
-        if (user.password !== login.password) {
+        if (user.password !== login.password){
             throw new UnauthorizedException("Datos incorrectos");
         }
 
@@ -138,66 +121,23 @@ async update(
     }
  
 
-    @Put()
-    @UseGuards(AuthGuard('jwt'))
-    public update(@Body() user: User, @Request() request) {
-
-        /*  if (request.user.role !== Role.ADMIN && user.id !== request.user.id) {
-     
-             throw new UnauthorizedException();
-         }
-     
-         return this.userRepository.save(user);
-     } */
-        if (request.user.role !== Role.ADMIN && user.id !== request.user.id) {
-
-            throw new UnauthorizedException();
-        }
-        return this.userRepository.save(user);
-    }
-
-
-    // account
-    @Get('account/:id')
-    @UseGuards(AuthGuard('jwt'))
-    public getCurrentAccountUser(@Request() request) {
-
-        return request.user;
-    }
-    // subir avatar
-    @Post('avatar')
-    @UseInterceptors(FileInterceptor('file'))
-    @UseGuards(AuthGuard('jwt'))
-    async uploadAvatar(
-        @UploadedFile() file: Express.Multer.File,
-        @Request() request
-    ) {
-        if (!file) {
-            throw new BadRequestException('Archivo incorrecto')
-        }
-
-        // guardar la ruta del archivo, hay que crear un atributo del user
-        request.user.photoUrl = file.filename;
-        return await this.userRepository.save(request.user);
-
-    }
 
     @Delete(':id')
     async deleteById(
         @Param('id', ParseIntPipe) id: number
     ) {
         const exists = await this.userRepository.existsBy({ id: id });
-
+    
         if (!exists) {
             throw new NotFoundException('User not found');
         }
-
+    
         try {
             await this.userRepository.delete(id);
         } catch (error) {
             console.log("Error al borrar el usuario", error);
             throw new ConflictException('No se puede borrar.');
         }
-
-    }
+    
+}
 }
