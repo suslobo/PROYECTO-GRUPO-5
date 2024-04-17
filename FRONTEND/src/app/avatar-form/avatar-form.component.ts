1956d64995f9d6ea57e2c0e8a23542db289125dd
almudenadomenech <violeta_33@hotmail.es>
2024-04-17 09:17:24 +0200

    componente avatar copiado

---
+++ FRONTEND/src/app/avatar-form/avatar-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './avatar-form.component.html',
+  styleUrl: './avatar-form.component.css'
+})
+export class AvatarFormComponent {
+
+}
